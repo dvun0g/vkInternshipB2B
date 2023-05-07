fe7d0e5f14810813cfca7669115faa2b9fd630e3
dvun0g <nikita.runikita.ru@gmail.com>
2023-05-07 13:10:51 +0300

    fix Правки после code review

--- config/helpers/pathResolve/pathResolve.ts
+++ config/helpers/pathResolve/pathResolve.ts
@@ -2,6 +2,9 @@ import { resolve } from 'path';
 
 import type { IPathResolve } from './pathResolve.typings';
 
+/**
+ * Функция для генерации путей от корневого каталога проекта
+ */
 export const pathResolve: IPathResolve = function (currentPath) {
 	return resolve(__dirname, '../../../', currentPath);
 };
